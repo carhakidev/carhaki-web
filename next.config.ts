abae476682f93a899c5b4e0bb1562b0fbcfa8aa2
CarHaki Dev <carhakidev@gmail.com>
2026-06-24 08:41:35 +0000

    chore: remove Vercel powered-by header, replace with CarHaki

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'x-powered-by',
+          value: 'CarHaki',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
